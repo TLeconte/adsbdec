5ef0e518365ed6b1968ee4014724b22f61f872bc
leconte <thierry.leconte35@free.fr>
2022-02-06 17:48:53 +0000

    clean and speed up demod

--- demod.c
+++ demod.c
@@ -28,10 +28,25 @@ int df = 0;
 extern int validShort(uint8_t *frame,const uint64_t ts,uint32_t pw);
 extern int validLong(uint8_t *frame, const uint64_t ts,uint32_t pw);
 
+static inline uint8_t getabyte(int idx) {
+	uint8_t bits=0;
+
+	if (ampbuff[idx] > ampbuff[idx + PULSEW]) bits |= 0x80;
+	if (ampbuff[idx + 2 * PULSEW] > ampbuff[idx + 3 * PULSEW]) bits |= 0x40;
+	if (ampbuff[idx + 4 * PULSEW] > ampbuff[idx + 5 * PULSEW]) bits |= 0x20;
+	if (ampbuff[idx + 6 * PULSEW] > ampbuff[idx + 7 * PULSEW]) bits |= 0x10;
+	if (ampbuff[idx + 8 * PULSEW] > ampbuff[idx + 9 * PULSEW]) bits |= 0x08;
+	if (ampbuff[idx + 10 * PULSEW] > ampbuff[idx + 11 * PULSEW]) bits |= 0x04;
+	if (ampbuff[idx + 12 * PULSEW] > ampbuff[idx + 13 * PULSEW]) bits |= 0x02;
+	if (ampbuff[idx + 14 * PULSEW] > ampbuff[idx + 15 * PULSEW]) bits |= 0x01;
+
+	return bits;
+}
+
+
 int deqframe(const int idx, const uint64_t sc)
 {
 	static uint32_t pv0, pv1, pv2;
-	int lidx=idx;
 
 	pv0 = pv1;
 	pv1 = pv2;
@@ -42,72 +57,63 @@ int deqframe(const int idx, const uint64_t sc)
 	     ampbuff[idx + 7 * PULSEW] + ampbuff[idx + 9 * PULSEW];
 
 	/* peak detection */
-	if (pv1 > pv0 && pv1 > pv2) 
-	{
+	if (pv1 > pv0 && pv1 > pv2) {
+		int lidx;
+		uint32_t ns;
 		uint8_t frame[14];
 		int flen;
-		uint8_t bits = 0;
 		int fmlen;
-		int k;
-		int ns;
 
 		lidx = idx - 1 ;
 
 		/* noise estimation */
-		ns = ampbuff[lidx + PULSEW] + ampbuff[lidx + 3 * PULSEW] + ampbuff[lidx + 8 * PULSEW] + ampbuff[lidx + 10 * PULSEW]; 
+		ns = ampbuff[lidx + PULSEW] + ampbuff[lidx + 3 * PULSEW] + ampbuff[lidx + 4 * PULSEW] + ampbuff[lidx + 5 * PULSEW] + ampbuff[lidx + 6 * PULSEW] + ampbuff[lidx + 8 * PULSEW] ; 
 
 		/* s/n test */
 		if (pv1 < 2*ns)  {
 			return 1;
 		}
 
-		/* decode each bit */
-		flen=0;fmlen=0;
-		for (k = 0; k <= 14*8; k++) {
+		lidx+=16*PULSEW;
 
-			if (k && k % 8 == 0) {
-				frame[flen] = bits;
-				flen++;
+		/* decode 1st byte to get len */
+		frame[0]=getabyte(lidx);
 
-				if (flen == 1) {
-					switch(frame[0] >> 3) {
-        					case 0: case 4: case 5: case 11:
-							fmlen=7;
-							if(df==0) {
-								pv1=pv2=0;
-								return 1;
-							}
-							break;
-        					case 16: case 17: case 18: case 20: case 21: case 24:
-							fmlen=14;
-							break;
-        					default:
-							pv1=pv2=0;
-							return 1;
-    					}
-				} 
-
-				if (fmlen == 7 && flen == 7) {
-					if (validShort(frame, sc, pv1)) {
-						pv1=pv2=0;
-						return 128 * PULSEW;
-					}
+		switch(frame[0] >> 3) {
+        		case 0: case 4: case 5: case 11:
+				fmlen=7;
+				if(df==0) {
+					pv1=pv2=0;
+					return 1;
 				}
+				break;
+        		case 16: case 17: case 18: case 20: case 21: case 24:
+				fmlen=14;
+				break;
+        		default:
+				pv1=pv2=0;
+				return 1;
+    		}
 
-				if (fmlen == 14 && flen == 14) {
-					if (validLong(frame,sc, pv1)) {
-						pv1=pv2=0;
-						return 240 * PULSEW;
-					}
-				}
-				bits = 0;
-			}
-
-			bits = bits << 1;
-			if (ampbuff[lidx + (16 + 2 * k) * PULSEW] >
-			    ampbuff[lidx + (17 + 2 * k) * PULSEW])
-				bits |= 1;
+		for(flen=1;flen<fmlen;flen++) {
+			lidx+=16*PULSEW;
+			frame[flen]=getabyte(lidx);
 		}
+
+		if (fmlen == 7 ) {
+			if (validShort(frame,sc,pv1)) {
+				pv1=pv2=0;
+				return 128 * PULSEW;
+			}
+		}
+
+		if (fmlen == 14 ) {
+			if (validLong(frame,sc,pv1)) {
+				pv1=pv2=0;
+				return 240 * PULSEW;
+			}
+		}
+
 	}
 	return 1;
 }
